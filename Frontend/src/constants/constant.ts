b978ea104c7990240ea8a8f4d34b3237f5b9471d
Huzaifa Mumtaz <99095017+huzaifa-88@users.noreply.github.com>
2023-12-10 22:36:38 +0500

    Update about me
    update about me section with different words

--- Frontend/src/constants/constant.ts
+++ Frontend/src/constants/constant.ts
@@ -4,10 +4,11 @@ import { NavItems } from "@/models/Header"
 export const userInfo: UserObject = {
     name: 'Huzaifa Mumtaz',
     picture : 'https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D',
-    heading : 'I&#39;m passionate Software developer and have expertise in developing and mainting software development life cycle.',
+    heading : 'Embracing the World of Computer Science: Merging Creativity and Logic. With a passion for technology, I craft solutions that blend innovation and functionality, navigating the digital landscape to create impactful and efficient solutions. Join me on this journey through algorithms and software engineering as we shape tomorrow"s possibilities today',
     about : `
-            <p>As a passionate software engineer, I thrive on crafting elegant solutions to complex problems, leveraging my expertise in software development. With a relentless pursuit of innovation and a dedication to quality code, I aim to drive impactful and efficient results, contributing to cutting-edge projects and pushing the boundaries of technology.</p>
-            <p>In 2021, my journey into the world of software engineering commenced, igniting my curiosity and passion for crafting innovative solutions. Since then, I've embarked on a fulfilling journey of learning, growth, and hands-on experiences, continuously evolving as a proficient software engineer driven by a fervent pursuit of knowledge and mastery in the field. </p>
+            <p>As a computer science enthusiast fascinated by the fusion of creativity and technology. I began my journey into coding with a keen interest in problem-solving and a quest for innovation. Exploring the realms of algorithms and software engineering, I thrive on crafting efficient, impactful solutions.</p>
+            <p>In 2021, I started my journey in the field of science. I'm always eager to learn and explore. This drive keeps me motivated to take on challenges, expand my knowledge, and reach new frontiers.</p>
+            <p>I have wide range of hobbies and passions that keep me busy. From reading, playing sports, watch movies, I am always seeking new experinces and love to keep myself engaged and learning new things.</p>
             `,
     skills : ['React', 'Next JS', 'Data Structures', 'Microsoft SQL', 'Desktop Applications', 'MongoDB', 'Python', 'C++', 'Win-UI3']
 }
