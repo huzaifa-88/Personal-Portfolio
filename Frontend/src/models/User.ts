56fbec0fcb8b506bf54e727a3361582630ac22b3
Huzaifa <huzaifamumtaz09@gmail.com>
2026-09-15 13:23:06 +0500

    setup: initialize new setup using bun and remove old code

--- Frontend/src/models/User.ts
+++
@@ -1,9 +0,0 @@
-
-// Validate user info in constant.ts file
-export interface UserObject {
-    name : string,
-    picture : string,
-    heading : string,
-    about : string,
-    skills : Array<string>
-}
